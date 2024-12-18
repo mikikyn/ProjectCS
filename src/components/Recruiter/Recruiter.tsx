24c0b0e79b2d3f9d965221aca20819a1887def59
mikikyn <baizhigit.akaev@gmail.com>
2024-12-18 13:36:32 +0600

    finally

---
+++ src/components/Recruiter/Recruiter.tsx
@@ -0,0 +1,8 @@
+import netherlandsRecruiter from './jeremy-akeze-doghouse-it-recruitment.jpg';
+import * as S from './style';
+
+const Recruiter = () => (
+ <div></div>
+);
+
+export default Recruiter;
